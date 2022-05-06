d12f4b1cd754a470d9039e8f1ced7fb8756a46ad
Will Gundy <willgunderson503@gmail.com>
2022-05-05 22:17:16 -0700

    Initial setup and got drivers displaying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getF1Drivers } from './services/fetch-utils';
 
 function App() {
+  // const [isLoading, setIsLoading] = useState(false);
+  const [f1Drivers, setF1Drivers] = useState([]);
+  
+  useEffect(async () => {
+    // setIsLoading(true);
+    
+    const f1DriversResponse = await getF1Drivers();
+
+    // setIsLoading(false);
+    setF1Drivers(f1DriversResponse);
+  }, []);
+
+  console.log(f1Drivers);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>F1 Drivers</h1>
+      {f1Drivers.map((driver, i) => <h3 key={i}>{driver.driver}</h3>)}
     </div>
   );
 }
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,37 @@
+import { client } from './client';
+
+export async function getPremierLeagueTeams() {
+  const response = await client
+    .from('premier_league_teams')
+    .select('*');
+
+  return response.body;
+}
+
+export async function getPremierLeaguePlayers() {
+  const response = await client
+    .from('premier_league_players')
+    .select('*')
+    .limit(20);
+  
+  return response.body;
+}
+
+export async function getPremierLeagueMatches() {
+  const response = await client
+    .from('premier_league_teams')
+    .select('*')
+    .limit(20);
+  
+  return response.body;
+}
+
+export async function getF1Drivers() {
+  const response = await client
+    .from('f1_drivers')
+    .select('*');
+    
+  return response.body;
+}
+    
+  
